2964190af3076e8f26eea549d46646d643ea30aa
Carl Caraan <bannedefused@gmail.com>
2023-02-21 22:41:06 +0800

    2/21/23

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css'
+// Page Components
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Search from "./pages/search/Search";
+import Recipe from "./pages/recipe/RecipeDetails";
+
+// Styles
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/recipe/RecipeDetails.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./RecipeDetails.css";
+
+function RecipeDetails() {
+  return <div>RecipeDetails</div>;
+}
+
+export default RecipeDetails;
