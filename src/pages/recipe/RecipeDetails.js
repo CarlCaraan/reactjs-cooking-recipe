92810f38bb7a5c2b3f0ebdc3f09ccf336b960910
Carl Caraan <bannedefused@gmail.com>
2023-02-23 09:20:26 +0800

    2/23/23

--- src/pages/recipe/RecipeDetails.js
+++ src/pages/recipe/RecipeDetails.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+
 import "./RecipeDetails.css";
 
 function RecipeDetails() {
-  return <div>RecipeDetails</div>;
+  const { id } = useParams();
+  const url = "http://localhost:3000/recipes/" + id;
+  const { error, isPending, data: recipe } = useFetch(url);
+
+  return (
+    <div className="recipe">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipe && (
+        <Fragment>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <p>{recipe.method}</p>
+        </Fragment>
+      )}
+    </div>
+  );
 }
 
 export default RecipeDetails;
