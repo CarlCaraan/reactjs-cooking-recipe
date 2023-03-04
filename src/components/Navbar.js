d3aa94ef153f0778abb3981f7527b33c3d1e8e66
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-04 21:50:45 +0800

    3/4/23

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+
+// Context API
+import { ThemeContext } from "../context/ThemeContext";
 
 // Styles
 import "./Navbar.css";
@@ -8,8 +11,10 @@ import "./Navbar.css";
 import Searchbar from "./Searchbar";
 
 function Navbar() {
+  const { color } = useContext(ThemeContext);
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Cooking Recipe</h1>
