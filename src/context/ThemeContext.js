d1a5f717bd85dd63fc35fd5d071030ce7af40a40
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-04 14:36:18 +0800

    3/4/23

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,6 +1,11 @@
 import { createContext } from "react";
 
 export const ThemeContext = createContext();
+
 export function ThemeProvider({ children }) {
-  return <ThemeContext.Provider>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={{ color: "blue" }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
