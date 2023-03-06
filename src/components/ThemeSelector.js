2cebb1b69a7beb3579c8a5d1a4f75adc35099f76
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-06 22:03:06 +0800

    3/6/23

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -3,12 +3,26 @@ import { useTheme } from "../hooks/useTheme";
 
 import React from "react";
 
+// Image
+import modeIcon from "../assets/mode-icon.svg";
+
 const themeColors = ["#58249c", "#249c6b", "#b70233"];
 
 function ThemeSelector() {
-  const { changeColor } = useTheme();
+  const { changeColor, changeMode, mode } = useTheme();
+  const toggleMode = () => {
+    changeMode(mode === "dark" ? "light" : "dark");
+  };
   return (
     <div className="theme-selector">
+      <div className="mode-toggle">
+        <img
+          src={modeIcon}
+          alt="sun-icon"
+          style={{ filter: mode === "dark" ? "invert(100%)" : "invert(20%)" }}
+          onClick={toggleMode}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map((color) => (
           <div
