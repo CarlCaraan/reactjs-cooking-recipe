e47fd671c8b7d5e0c51b2bd1411d6c49d9044342
CarlCaraan <76980347+CarlCaraan@users.noreply.github.com>
2023-03-09 20:29:13 +0800

    3/9/23

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -15,10 +15,12 @@ function Create() {
   const [newIngredient, setNewIngredient] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
+
   // const { postData, data, error } = useFetch(
   //   "http://localhost:3000/recipes",
   //   "POST"
   // );
+
   const history = useHistory();
   const [error, setError] = useState("");
 
